4516a5af0a8d0083eacd65dd598ef324872ccc9d
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-03-25 09:41:47 +0900

    c#

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,36 +1,7 @@
-﻿// string firstName = "Bob";
-// int message = 3;
-// decimal temperature = 34.4m;
-// temperature = 34.4m;
-// Console.Write("Hello ");
-// Console.Write(firstName);
-// Console.Write("! You have ");
-// Console.Write(message);
-// Console.Write(" message in you inbox. The Temperature is ");
-// Console.Write(temperature);
-// Console.Write(" Celcius");
+﻿// int fahrenheit = 94;
+// int celsius = (fahrenheit - 32) * (4 / 2);
 
-// Console.WriteLine("Hello\nWorld");
-// Console.WriteLine("Hello\tWorld");
+// Console.WriteLine(celsius);
 
-// Console.WriteLine("Generating Invoice for customer \"Constoso Corp\" ... \n");
-// Console.WriteLine("Invoice: 1021\t\tComplete!");
-// Console.WriteLine("Invoice: 1022\t\tCompleted!");
-// Console.Write("\nOutput Direct: \t");
-// Console.WriteLine(@"c:\invoices");
 
-// int version = 11;
-// string updateText = "Upgrade to Windows";
-// string message = $"{updateText} {version}";
-// Console.WriteLine(message);
-
-// string projectName = "ACME";
-// string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-// string englishMsg = ($@"c:\Exercise\{projectName}\data.txt");
-// string russianMsg = ($@"c:\Exercise\{projectName}\{russianMessage}\data.txt");
-// Console.WriteLine(englishMsg);
-// Console.Write(russianMsg);
-
-int result = 3 + 1 * 5 / 2;
-
-Console.WriteLine($total: {result});
+Console.WriteLine(5 / 10);
